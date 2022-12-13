2515e9194ba99b137d8b9f4952eaf2b5817389a2
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-12-13 11:08:10 +0500

    add more componenrts and styled them

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,7 +3,7 @@ import { graphql, compose } from 'react-apollo';
 import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
 class AddBook extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
         this.state = {
             name: '',
@@ -11,17 +11,17 @@ class AddBook extends Component {
             authorId: ''
         };
     }
-    displayAuthors() {
+    displayAuthors(){
         var data = this.props.getAuthorsQuery;
-        if (data.loading) {
-            return (<option disabled>Loading authors</option>);
+        if(data.loading){
+            return( <option disabled>Loading authors</option> );
         } else {
             return data.authors.map(author => {
-                return (<option key={author.id} value={author.id}>{author.name}</option>);
+                return( <option key={ author.id } value={author.id}>{ author.name }</option> );
             });
         }
     }
-    submitForm(e) {
+    submitForm(e){
         e.preventDefault()
         // use the addBookMutation
         this.props.addBookMutation({
@@ -30,25 +30,25 @@ class AddBook extends Component {
                 genre: this.state.genre,
                 authorId: this.state.authorId
             },
-            refetchQueries:[{query: getBooksQuery}]
+            refetchQueries: [{ query: getBooksQuery }]
         });
     }
-    render() {
-        return (
-            <form id="add-book" onSubmit={this.submitForm.bind(this)} >
+    render(){
+        return(
+            <form id="add-book" onSubmit={ this.submitForm.bind(this) } >
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" onChange={(e) => this.setState({ name: e.target.value })} />
+                    <input type="text" onChange={ (e) => this.setState({ name: e.target.value }) } />
                 </div>
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" onChange={(e) => this.setState({ genre: e.target.value })} />
+                    <input type="text" onChange={ (e) => this.setState({ genre: e.target.value }) } />
                 </div>
                 <div className="field">
                     <label>Author:</label>
-                    <select onChange={(e) => this.setState({ authorId: e.target.value })} >
+                    <select onChange={ (e) => this.setState({ authorId: e.target.value }) } >
                         <option>Select author</option>
-                        {this.displayAuthors()}
+                        { this.displayAuthors() }
                     </select>
                 </div>
                 <button>+</button>
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,31 +1,39 @@
-import React, { Component } from "react";
-import { graphql } from "react-apollo";
-import { getBooksQuery } from "../queries/queries";
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { getBooksQuery } from '../queries/queries';
 
-class BookList extends Component {  
-    displayBooks(){
-      var data = this.props.data;
-      if(data.loading){
-        return(
-          <div>Loading Books...</div>
-        )
-      } else {
-        return data.books.map(book=>{
-          return(
-            <li key={book.id}>{book.name}</li>
-          )
-        })
-      }
+// components
+import BookDetails from './BookDetails';
+
+class BookList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selected: null
+        }
     }
-    render() {
-    return (
-      <div>
-        <ul id="book-list">
-          {this.displayBooks()}
-        </ul>
-      </div>
-    );
-  }
+    displayBooks(){
+        var data = this.props.data;
+        if(data.loading){
+            return( <div>Loading books...</div> );
+        } else {
+            return data.books.map(book => {
+                return(
+                    <li key={ book.id } onClick={ (e) => this.setState({ selected: book.id }) }>{ book.name }</li>
+                );
+            })
+        }
+    }
+    render(){
+        return(
+            <div>
+                <ul id="book-list">
+                    { this.displayBooks() }
+                </ul>
+                <BookDetails bookId={ this.state.selected } />
+            </div>
+        );
+    }
 }
 
 export default graphql(getBooksQuery)(BookList);
